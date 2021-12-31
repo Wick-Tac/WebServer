3e73ad989c7041a60116042326c00588febf6910
Alejandro Flores Jacobo <87666446+Wick-Tac@users.noreply.github.com>
2021-12-30 20:04:09 -0700

    Probando los ejercicios

--- expressProyecto/index.js
+++ expressProyecto/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/html/index.html'));
- 
+    //res.send("hola mundo");
 });
 
 app.get('/saludo', function (req, res) {
--- http/index.js
+++ http/index.js
@@ -7,6 +7,7 @@ const port = 8000;
 const requestListener = function (req, res) {
     res.setHeader("Content-Type", "text/html");
     res.end('<html><body><h1>This is HTML</h1></body></html>');
+    //res.end("mi primer servidor");
 };
 const server = http.createServer(requestListener);
 server.listen(port, host, () => {
